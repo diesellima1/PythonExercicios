79b4feae22d737b876441b95454f07441e5c6357
adevilson_delima <adevilson_delima@hotmail.com>
2025-01-26 18:01:26 -0300

    ex003

---
+++ ex0000_commit-githab-comandos.py
@@ -0,0 +1,10 @@
+#abra o terminal "alt+F12"
+
+#comandos:
+
+#git add .
+#git commit -m "ex001"
+#git push origin master
+
+#dentro de aspas "ex001" descreva a sua atualização para saber quando e o que foi atualizado!
+# ex001 é a único valor a ser alterado os outros comandos não mudam !!!
