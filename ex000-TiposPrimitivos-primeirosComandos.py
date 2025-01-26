0addcced8a220ade40d018efbb74bbfe25cec05e
adevilson_delima <adevilson_delima@hotmail.com>
2025-01-26 17:47:42 -0300

    ex001

---
+++ ex000-TiposPrimitivos-primeirosComandos.py
@@ -0,0 +1,18 @@
+'''int  é tipo primitivo para não deixar os números virar string, sem o int ele nuta os
+números formando 22 se digitar  duas vezes o dois para somar 4'''
+
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite mais um número:'))
+s = n1 + n2
+print('A soma vale',s)
+
+
+#   tipos de primitivos
+
+#int() números inteiros: 7; -4; 0; 9875;
+
+#float() ponto flutuante "Númeors com pontos ou númeroS reais!": 4.5; 0.076; 7.0;
+
+#bool() "Boolenos" só aceita Verdaeiro ou Falso: True or False
+
+#str() "String" é uma palavra dendro de aspas" 'Olá'; 'texto' '' "string vazia abre e fecha aspas"
---
+++ ex000_Fformart.py
@@ -0,0 +1,10 @@
+' ' ' FORMATA{} É MUITO MELHOR USAR QUE O PRINT DIRETO  ' ' '
+#sintaxe pytohon3
+#print('A soma vale',s)
+#print('A soma vale{}'.format(S))
+
+
+#exemplo funcinal
+S = 10  # Por exemplo, o valor de S
+print('A soma vale {}'.format(S))
+
