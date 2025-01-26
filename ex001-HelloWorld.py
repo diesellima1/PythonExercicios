5a16270be85117506e3b27aaff00cec4655c84d9
adevilson_delima <adevilson_delima@hotmail.com>
2025-01-26 10:48:26 -0300

    primeiro commit python ex000

---
+++ ex001-HelloWorld.py
@@ -0,0 +1,6 @@
+'''Criar um programa que escreva "Olá mundo!" na tela. print('Olá, Mundo !') de com variável'''
+
+msg = 'Olá, Mundo!'
+print(msg)
+
+#print('Hello, world!')
---
+++ ex002-input-formulario-format.py
@@ -0,0 +1,39 @@
+#digite o seu nome
+
+
+nome = input('Digite Seu Nome:')
+print('É um prazer te conhecer,',nome)
+
+idade = input('Qual a sua idade?')
+print(idade,",muito jovem ainda.")
+
+cidade = input('Qual a sua cidade?')
+print(cidade,'parece ser muito bonita!')
+
+agradecimento = input('Diga a Deus!')
+print(agradecimento,'!!!Tchau amigo!!!')
+
+formatName = input('Digite seu nome:')
+print('essa é outra forma de fazer o código, aqui usamos a função format,{{}} {}!'.format(formatName))
+
+
+# Ask for the user's name
+# name = input('Enter Your Name:')
+# Print a greeting message with the user's name
+# print('It is a pleasure to meet you,', name)
+
+# Ask for the user's age
+# age = input('What is your age?')
+# Print a message commenting on the user's age
+# print(age, ", very young still.")
+
+# Ask for the user's city
+# city = input('What is your city?')
+# Print a message commenting on the user's city
+# print(city, 'seems to be very beautiful!')
+
+# Ask the user to say goodbye
+# farewell = input('Say Goodbye!')
+# Print a goodbye message
+# print(farewell, '!!!Bye friend!!!')
+
