d464221bc89984ea7f587c8396459a93c548597d
adevilson_delima <adevilson_delima@hotmail.com>
2025-01-29 17:46:11 -0300

    ex007

--- ex011-calculo-de-area-de-paredes-tinta-m.py
+++ ex011-calculo-de-area-de-paredes-tinta-m.py
@@ -1,3 +1,11 @@
 #Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a
 #quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta,
  #pinta uma área de 2m quadradados
+
+largura = float(input('Largura da parede?'))
+altura = float(input('Altura da parede?'))
+area = altura * largura
+litros_tinta = area / 2
+
+print('sua parede tem a dimensão de {} x {} e sua área é de {}m\u00B2 \nPara pintar essa parede, você precisa de {}l de tinta'.format(largura, altura, area, litros_tinta))
+#u00B2 significa dois elevado "m²"
